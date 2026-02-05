739324d9d4020658d344f7a059a79bc1ccc01102
Wanzhen Xie <wendyxie@wanzhens-air.mynetworksettings.com>
2026-02-05 00:10:57 -0500

    wakeup-03: Add DOM manipulation

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+const calcBtn = document.getElementById("calc-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div"); // 👀 Add this
+
 function calcWakeUpTimes() {
   const fallAsleepTime = new Date();
   fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
   const wakeUpTime = new Date(fallAsleepTime);
-  const wakeUpTimes = [];
+  wakeUpHoursDiv.innerHTML = ""; // 👀 Clear previous results
 
   for (let i = 1; i <= 6; i++) {
     wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
     const timeString = wakeUpTime.toLocaleTimeString("en-US", {
       timeStyle: "short",
     });
-    wakeUpTimes.push(timeString);
-  }
 
-  console.log("Wake-up times:", wakeUpTimes.join(", "));
+    // 👀 Create a div for each time
+    const cycleDiv = document.createElement("div");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = timeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
+  }
 }
 
-const calcBtn = document.getElementById("calc-btn");
+
 calcBtn.onclick = calcWakeUpTimes;
+
+
+
 
 
